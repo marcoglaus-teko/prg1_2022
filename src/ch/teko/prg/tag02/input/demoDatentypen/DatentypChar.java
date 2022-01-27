abf7034a283435255ef445e47894b7418772ccf3
marcoglaus-teko <marco.glaus@edu.teko.ch>
2022-01-27 16:13:40 +0100

    input tag02

---
+++ src/ch/teko/prg/tag02/input/demoDatentypen/DatentypChar.java
@@ -0,0 +1,14 @@
+package ch.teko.prg.tag02.input.demoDatentypen;
+
+public class DatentypChar {
+    public static void main(String[] args) {
+        char a = 65;
+        char b = 'a';
+
+        System.out.println(a);
+        System.out.println(b);
+
+        System.out.println((int)a);
+        System.out.println((int)b);
+    }
+}
