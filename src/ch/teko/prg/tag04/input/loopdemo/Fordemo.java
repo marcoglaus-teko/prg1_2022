b0879db18d8a26c55cfbd8125281ffe9bf7e0c7d
marcoglaus-teko <marco.glaus@edu.teko.ch>
2022-02-10 15:28:11 +0100

    input tag04

---
+++ src/ch/teko/prg/tag04/input/loopdemo/Fordemo.java
@@ -0,0 +1,9 @@
+package ch.teko.prg.tag04.input.loopdemo;
+
+public class Fordemo {
+    public static void main(String[] args) {
+        for(int i=1; i<11; i++){
+            System.out.println("Count is: " + i);
+        }
+    }
+}
---
+++ src/ch/teko/prg/tag04/input/loopdemo/Whiledemo.java
@@ -0,0 +1,19 @@
+package ch.teko.prg.tag04.input.loopdemo;
+
+public class Whiledemo {
+    public static void main(String[] args) throws InterruptedException {
+        int count = 1;
+        while (count < 11) {
+            System.out.println("Count is: " + count);
+            count++;
+        }
+
+        // Endlosschleife --> Programm abbrechen :)
+        int i = 0;
+        while (true) {
+            System.out.println(i * i);
+            Thread.sleep(1000);
+            i++;
+        }
+    }
+}
