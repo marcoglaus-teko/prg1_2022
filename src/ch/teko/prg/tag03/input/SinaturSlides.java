90511f18b70005c738f6b6e51cdeb4bc99ef74cc
marcoglaus-teko <marco.glaus@edu.teko.ch>
2022-02-10 22:08:36 +0100

    signature example

---
+++ src/ch/teko/prg/tag03/input/SinaturSlides.java
@@ -0,0 +1,22 @@
+package ch.teko.prg.tag03.input;
+
+public class SinaturSlides {
+    public void sampleMethode(int x, int y) {
+        System.out.println(x + y);
+    }
+
+    /*
+    public int sampleMethode(int x, int y) {
+        System.out.println(x+y);
+        return x+y;
+    }
+     */
+
+    public void sampleMethode(short x, int y) {
+        System.out.println(x + y);
+    }
+
+    public void sampleMethode(int x, int y, int z) {
+        System.out.println(x + y);
+    }
+}
