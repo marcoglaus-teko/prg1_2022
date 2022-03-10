a684d84f246d249018eb3fb011d0136d5c93c362
marcoglaus-teko <marco.glaus@edu.teko.ch>
2022-03-10 16:16:58 +0100

    input tag06

---
+++ src/ch/teko/prg/tag06/input/beispiel1/Circle.java
@@ -0,0 +1,7 @@
+package ch.teko.prg.tag06.input.beispiel1;
+
+public class Circle extends Shape {
+    public Circle() {
+        //super();
+    }
+}
---
+++ src/ch/teko/prg/tag06/input/beispiel2/Circle.java
@@ -0,0 +1,7 @@
+package ch.teko.prg.tag06.input.beispiel2;
+
+public class Circle extends Shape {
+    public Circle(String color) {
+        super(color);
+    }
+}
